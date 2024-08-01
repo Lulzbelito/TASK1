f7e6707bca1aabf017909f7dbfdcba791a19cece
Semprini <frsempri@med.umich.edu>
2024-08-01 15:48:23 -0400

    First commit

---
+++ Fibonacci.js
@@ -0,0 +1,11 @@
+do {
+    var numberProvided = Number(prompt("Provide number"))
+    console.log(isNaN(numberProvided))
+    console.log(numberProvided.length)
+} while (isNaN(numberProvided));
+
+
+function fibonacciF() {
+
+}
+
