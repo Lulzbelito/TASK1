dd0aa0d0e7003dd35cd30acb0f4e1b2e24c891d3
Semprini <frsempri@med.umich.edu>
2024-08-01 16:31:21 -0400

    2nd push

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,25 @@
+var numberProvided
+
+var startNumber = [0, 1]
+var saved
+
 do {
-    var numberProvided = Number(prompt("Provide number"))
+    numberProvided = Number(prompt("Provide number"))
     console.log(isNaN(numberProvided))
     console.log(numberProvided.length)
 } while (isNaN(numberProvided));
 
 
-function fibonacciF() {
+function fibonacciF(startNumber, saved) {
+    for (let index = 0; index < numberProvided; index++) {
+        console.log(startNumber[0])
+        saved = startNumber[1]
+        startNumber[1] = startNumber[0] + startNumber[1]
+        startNumber[0] = saved
 
+    }
 }
+
+fibonacciF(startNumber)
+
 
